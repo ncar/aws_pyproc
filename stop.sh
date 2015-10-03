9a31e3f71e7a65f9d04450cc1fc83ccb99a8a81a
nicholascar <sa.aws.server@gmail.com>
2015-10-03 22:27:20 +0930

    stop.py changed PID file name

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #sudo kill `ps aux | grep procdeamon | grep -v "grep" | head -3 | awk '{print $2}'`
-kill `more /opt/procdeamon/procdeamon.pid`
-rm /opt/procdeamon/procdeamon.pid
+kill `more /opt/processor/procdeamon.pid`
+rm /opt/processor/procdeamon.pid
 echo "procdeamon stopped"
